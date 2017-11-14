50a26da9500f656cc0abcca9b1c70aa680bc1d1d
Lobunko Evgenii <taktreba2017@gmail.com>
2017-11-14 14:03:17 +0200

    try do first commit from phpshtorm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! Faggot!
                 </div>
             </div>
         </div>
