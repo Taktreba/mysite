9be24511048b3efd3b7327e5af878a02748b5108
Lobunko Evgenii <taktreba2017@gmail.com>
2017-11-14 18:05:15 +0200

    commit before create table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="links">
+                            <a href="staff">Состав</a>
                         </div>
-                    @endif
-
-                    You are logged in! Faggot!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
